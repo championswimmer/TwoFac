136b16aad3dab00013699751909985051e3d76aa
Arnav Gupta <dev@championswimmer.in>
2025-07-02 00:44:04 +0100

    feat: implement animated display for account OTPs - Added animation to the display command for showing account OTPs. - Created a new method to generate the display table with progress indicators. - Exits the display command after 2 minutes of inactivity with a warning.
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- cliApp/src/commonMain/kotlin/tech/arnav/twofac/cli/commands/DisplayCommand.kt
+++ cliApp/src/commonMain/kotlin/tech/arnav/twofac/cli/commands/DisplayCommand.kt
@@ -10,8 +10,8 @@ import com.github.ajalt.mordant.rendering.BorderType
 import com.github.ajalt.mordant.rendering.TextColors
 import com.github.ajalt.mordant.rendering.TextStyles
 import com.github.ajalt.mordant.table.Borders
-import com.github.ajalt.mordant.table.Table
 import com.github.ajalt.mordant.table.table
+import com.github.ajalt.mordant.terminal.warning
 import com.github.ajalt.mordant.widgets.Padding
 import com.github.ajalt.mordant.widgets.ProgressBar
 import kotlinx.coroutines.delay
@@ -19,8 +19,10 @@ import kotlinx.coroutines.runBlocking
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import tech.arnav.twofac.cli.viewmodels.AccountsViewModel
+import tech.arnav.twofac.cli.viewmodels.DisplayAccountsStatic
 import kotlin.system.exitProcess
 import kotlin.time.Clock
+import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.ExperimentalTime
 
@@ -33,54 +35,59 @@ class DisplayCommand : CliktCommand(), KoinComponent {
 
     val passkey by option("-p", "--passkey", help = "Passkey to display").prompt("Enter passkey", hideInput = true)
 
-
-    @OptIn(ExperimentalTime::class)
     override fun run() = runBlocking {
         // Unlock the library with the provided passkey
         accountsViewModel.unlock(passkey)
 
         // Fetch and display all account OTPs
-        val accountOTPs = accountsViewModel.showAllAccountOTPs()
-        fun getTable() = table {
-            borderType = BorderType.SQUARE_DOUBLE_SECTION_SEPARATOR
-            header {
-                style = TextStyles.dim + TextColors.brightBlue
-                row("Account", "OTP", "Validity")
-            }
-            body {
-                column(1) {
-                    style = TextStyles.bold + TextColors.brightCyan
-                    cellBorders = Borders.ALL
-                    padding = Padding(0, 1, 0, 1) // Add padding to the cell
-                }
-                accountOTPs.forEach { (account, otp) ->
-                    val currentTimeSec = Clock.System.now().epochSeconds
-                    val elapsedTimeSec = currentTimeSec - (account.nextCodeAt - 30)
-                    val leftTimeSec = 30 - elapsedTimeSec
-                    row {
-                        cell(account.accountLabel)
-                        cell(otp.split("").joinToString(" "))
-                        cell(
-                            ProgressBar(
-                                total = 30L,
-                                completed = elapsedTimeSec,
-                                width = 15,
-                                separatorChar = "$leftTimeSec"
-                            )
-                        )
-                    }
-                }
-            }
+        val animation = terminal.animation<DisplayAccountsStatic> { displayAccounts ->
+            createTable(displayAccounts)
         }
 
-        val animation = terminal.animation<Table> {
-            getTable()
-        }
-
-        repeat(30) {
-            animation.update(getTable())
+        repeat(2.minutes.inWholeSeconds.toInt()) {
+            animation.update(accountsViewModel.showAllAccountOTPs())
             delay(1.seconds.inWholeMilliseconds)
         }
+        terminal.warning("Exiting display command after 2 minutes of inactivity.")
         exitProcess(0)
     }
+
+    @OptIn(ExperimentalTime::class)
+    private fun createTable(displayAccounts: DisplayAccountsStatic) = table {
+        borderType = BorderType.SQUARE_DOUBLE_SECTION_SEPARATOR
+        header {
+            style = TextStyles.dim + TextColors.brightBlue
+            row("Account", "OTP", "Validity")
+        }
+        body {
+            column(1) {
+                style = TextStyles.bold + TextColors.brightCyan
+                cellBorders = Borders.ALL
+                padding = Padding(0, 1, 0, 1) // Add padding to the cell
+            }
+            displayAccounts.forEach { (account, otp) ->
+                val currentTimeSec = Clock.System.now().epochSeconds
+                val elapsedTimeSec = currentTimeSec - (account.nextCodeAt - 30)
+                val leftTimeSec = 30 - elapsedTimeSec
+                row {
+                    cell(account.accountLabel)
+                    cell(otp.split("").joinToString(" "))
+                    cell(
+                        ProgressBar(
+                            total = 30L,
+                            completed = elapsedTimeSec.coerceIn(0L..30L),
+                            width = 15,
+                            separatorChar = "$leftTimeSec"
+                        )
+                    )
+                }
+            }
+        }
+        footer {
+            cellBorders = Borders.NONE
+            style = TextStyles.dim + TextColors.brightGreen
+            row("Press Ctrl-C key to exit")
+        }
+    }
+
 }
