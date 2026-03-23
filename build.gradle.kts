fcdeb1dc7b77d6efbba31fb58c8d9fd2d35f4c0a
Arnav Gupta <dev@championswimmer.in>
2026-03-23 01:42:15 +0000

    v1.4.1
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,5 +11,5 @@ plugins {
 }
 
 // Centralized app versioning — all subprojects inherit from here
-extra["appVersionCode"] = 260201040 // eg: 2026 02(Feb) 01 00 3 (1.0.3)
-extra["appVersionName"] = "1.4.0"
+extra["appVersionCode"] = 260201041 // eg: 2026 02(Feb) 01 00 3 (1.0.3)
+extra["appVersionName"] = "1.4.1"
