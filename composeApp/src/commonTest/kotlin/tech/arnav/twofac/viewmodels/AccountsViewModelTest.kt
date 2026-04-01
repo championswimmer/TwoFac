bc49296ec5c5eb5f21b486499f27290415820ddb
Arnav Gupta <dev@championswimmer.in>
2026-04-01 01:03:43 +0100

    test(composeApp): scaffold AccountsViewModelTest

---
+++ composeApp/src/commonTest/kotlin/tech/arnav/twofac/viewmodels/AccountsViewModelTest.kt
@@ -0,0 +1,11 @@
+package tech.arnav.twofac.viewmodels
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AccountsViewModelTest {
+    @Test
+    fun `accounts are properly parsed on initial load`() {
+        // TODO: setup AccountsViewModel with FakeTwoFacLib and assert accounts size/parsing
+    }
+}
