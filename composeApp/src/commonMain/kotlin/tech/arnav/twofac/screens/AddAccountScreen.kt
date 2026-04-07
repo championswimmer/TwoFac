958ac809d082836f675c59fd1964243977834434
Arnav Gupta <dev@championswimmer.in>
2026-04-08 00:12:26 +0100

    add screenshots and make home screen scrollable
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

--- composeApp/src/commonMain/kotlin/tech/arnav/twofac/screens/AddAccountScreen.kt
+++ composeApp/src/commonMain/kotlin/tech/arnav/twofac/screens/AddAccountScreen.kt
@@ -1,5 +1,7 @@
 package tech.arnav.twofac.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +9,8 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.Button
@@ -25,6 +29,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.stringResource
 import twofac.composeapp.generated.resources.Res
@@ -73,6 +78,8 @@ fun AddAccountScreen(
 
     val isLoading by viewModel.isLoading.collectAsState()
     val error by viewModel.error.collectAsState()
+    val focusManager = LocalFocusManager.current
+    val scrollState = rememberScrollState()
 
     // Pre-resolve localizable error strings for use in coroutine lambdas
     val errorClipboardPermission = stringResource(Res.string.add_account_error_clipboard_permission)
@@ -133,9 +140,14 @@ fun AddAccountScreen(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(paddingValues)
-                    .padding(16.dp),
+                    .padding(16.dp)
+                    .verticalScroll(scrollState)
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() },
+                    ) { focusManager.clearFocus() },
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically)
+                verticalArrangement = Arrangement.spacedBy(16.dp),
             ) {
                 OtpUriInputField(
                     value = uriText,
